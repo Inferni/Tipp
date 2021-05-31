0a143bfa6a4c5b937bd7baca41abd03ed0ed36c7
Rik Dortmans <rikdortmans@gmail.com>
2021-05-31 13:18:52 +0200

    Routing has been fixed

--- TIPP/Client/Models/User.cs
+++ TIPP/Client/Models/User.cs
@@ -1,9 +1,12 @@
-﻿namespace TIPP.Client.Models
+﻿using TIPP.Shared;
+
+namespace TIPP.Client.Models
 {
     public class User
     {
         public string Id { get; set; }
         public string Username { get; set; }
+        public Role Role { get; }
         public string Token { get; set; }
     }
 }
